fe14556559bd9d478a506c00086338972529fa03
berkanyldrim <berkanyldrim@gmail.com>
2024-07-28 22:46:23 +0300

    feat:axios interceptors created

---
+++ src/services/axios.ts
@@ -0,0 +1,16 @@
+// --- Axios Import ---
+import axios from "axios";
+// --- Store Import ---
+import authStore from "../store/modules/auth";
+
+//NOTE - Axios Default Url
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+
+axios.interceptors.request.use(function (config) {
+  if (config.url != "oauth/token") {
+    config.headers["Authorization"] = `Bearer ${
+      authStore?.getters && authStore.getters.token
+    }`;
+  }
+  return config;
+});
