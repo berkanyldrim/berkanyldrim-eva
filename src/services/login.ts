07c7677632ce419c657043d91401396763789ada
berkanyldrim <berkanyldrim@gmail.com>
2024-07-29 00:54:04 +0300

    chore:login service completed

--- src/services/login.ts
+++ src/services/login.ts
@@ -1,10 +1,26 @@
 // --- Axios Import ---
-import axios from "axios";
+import axios from "../services/axios";
 // --- Types Import ---
 import { ILogin } from "../types/login";
 // --- Store Import ---
-import authStore from "../store/modules/auth";
+import store from "../store/index";
 
-const login = (values: ILogin) => {
-  axios.post("oauth/token", values);
+export const login = async (values: ILogin) => {
+  await axios
+    .post("oauth/token", values)
+    .then((response) => {
+      if (response.status === 200) {
+        console.log(response.data.Data);
+        const tokenData = {
+          token: response.data.Data.AccessToken,
+          refreshToken: response.data.Data.RefreshToken,
+        };
+        store.dispatch("auth/login", tokenData);
+        return response;
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      throw error; // Hatanın dışarıya fırlatılması
+    });
 };
