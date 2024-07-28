9853ce1b062066b04c33170dc979cd6279a52f37
berkanyldrim <berkanyldrim@gmail.com>
2024-07-28 22:45:05 +0300

    chore:vuex , primevue,vue-router added

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,28 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+// --- Vue 3 Import ---
+import { createApp } from "vue";
+// --- Main Component Import ---
+import App from "./App.vue";
+// --- Vuex Store Import ---
+import store from "./store";
+// --- Router Import ---
+import router from "./router";
+// --- PrimeVue Import ---
+import PrimeVue from "primevue/config";
+import "primevue/resources/themes/aura-dark-blue/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeflex/primeflex.css";
+import "primeicons/primeicons.css";
+// --- PrimeVue Components Import ---
+import Button from "primevue/button";
+import InputText from "primevue/inputtext";
+import Password from "primevue/password";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(PrimeVue, { ripple: true });
+
+app.use(router);
+app.use(store);
+app.mount("#app");
+app.component("Button", Button);
+app.component("InputText", InputText);
+app.component("Password", Password);
