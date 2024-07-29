b3b6db6c103b03882f1742327d96c89d3657db64
berkanyldrim <berkanyldrim@gmail.com>
2024-07-30 00:36:06 +0300

    chore: persisted state added

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,12 @@
 import { createStore } from "vuex";
 import auth from "./modules/auth";
 import user from "./modules/user";
+import createPersistedState from "vuex-persistedstate";
 
 export default createStore({
   modules: {
     auth,
     user,
   },
+  plugins: [createPersistedState()],
 });
